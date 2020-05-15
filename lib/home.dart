88cd03642079e72ab515b5ef2a609bc01a8faae1
yuki-yuki-engineer <yuki.yuki.engineer@gmail.com>
2020-05-15 10:03:55 +0900

    implement custom icon

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -39,18 +38,49 @@ class _HomePageState extends State<Home> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            title: Text('ヒストリ'),
+            icon: Icon(
+              Icons.list,
+              size: 40,
+            ),
+            title: Text(
+              'メニューリスト',
+              style: TextStyle(fontSize: 11),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('材料'),
+            icon: Icon(
+              Icons.history,
+              size: 40,
+            ),
+            title: Text(
+              'ヒストリ',
+              style: TextStyle(fontSize: 12),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('手順'),
+            icon: Icon(
+              const IconData(0xe800, fontFamily: 'Material'),
+              color: Color.fromRGBO(105, 105, 105, 1.0),
+              size: 40,
+            ),
+            title: Text(
+              '材料',
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              const IconData(0xe800, fontFamily: 'Knife'),
+              color: Color.fromRGBO(105, 105, 105, 1.0),
+              size: 40,
+            ),
+            title: Text(
+              '手順',
+              style: TextStyle(fontSize: 12),
+            ),
           ),
         ],
 //        currentIndex: _selectedIndex,

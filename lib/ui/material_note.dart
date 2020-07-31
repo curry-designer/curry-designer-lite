7286616f1956b4f9013bc1413ef83caca322ca5d
kotani <s.kotani.49@gmail.com>
2020-08-01 08:35:39 +0900

    update

--- lib/ui/material_note.dart
+++ lib/ui/material_note.dart
@@ -3,26 +3,21 @@ import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 
-class TextStore with ChangeNotifier{
+class TextStore with ChangeNotifier {
   final materialList = [];
-
 }
 
-class MaterialNote extends StatelessWidget{
+class MaterialNote extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ChangeNotifierProvider<TextStore>(
-      create: (context) => TextStore(),
-      child: _MaterialNote()
-    );
+        create: (context) => TextStore(), child: _MaterialNote());
   }
 }
 
-
 class _MaterialNote extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -35,11 +30,8 @@ class _MaterialNote extends StatelessWidget {
         title: Text('材料'),
       ),
       body: TextWidget(),
-
     );
-
   }
-  
 }
 
 class TextWidget extends StatefulWidget {
@@ -50,27 +42,44 @@ class TextWidget extends StatefulWidget {
   _TextWidgetState createState() => _TextWidgetState();
 }
 
-class _TextWidgetState extends State<TextWidget>{
+class _TextWidgetState extends State<TextWidget> {
   GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
+  List<String> added = [];
+  String currentText = "";
 
-  var suggestions = ['クミン','コリアンダー','ターメリック','カイエンペッパー','ガラムマサラ'];
+  var suggestions = ['クミン', 'コリアンダー', 'ターメリック', 'カイエンペッパー', 'ガラムマサラ',
+  'cumin', 'coriander'];
   @override
-  Widget build(BuildContext context){
-    
-        return Scaffold(
-          body: ListView(
-            children: <Widget>[
-              SimpleAutoCompleteTextField(
-                key: key,
-                suggestions: suggestions,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: '材料をメモ',
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+                SimpleAutoCompleteTextField(
+          key: key,
+          suggestions: suggestions,
+          clearOnSubmit: true,
+          textSubmitted: (text) => setState(() {
+            if (text != "") {
+              added.add(text);
+            }
+          }),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: '材料をメモ',
           ),
-        ],
-      )
-    );
+        ),
+        Text('ここにリストだしたい'),
+            LimitedBox(
+              maxHeight: 100,
+              child:
+            ListView.builder(
+              itemCount: added.length,
+              itemBuilder: (BuildContext context,int index){
+                return Text(added[index]);
+            }))
+            ,
+
+      ],
+    ));
   }
-  
 }
